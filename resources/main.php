dfcaa653f770cb12c5fc557d52e2e530abd641ae
Anastasia Sannikova <ronja_ss@mail.ru>
2022-02-19 23:34:42 +0300

    ajax

--- resources/main.php
+++ resources/main.php
@@ -1,3 +1,15 @@
-<?
-var_dump($_POST);
+<?php
+
+if (isset($_POST["name"]) && isset($_POST["phonenumber"]) ) { 
+
+	// Формируем массив для JSON ответа
+    $result = array(
+    	'name' => $_POST["name"],
+    	'phonenumber' => $_POST["phonenumber"]
+    ); 
+
+    // Переводим массив в JSON
+    echo json_encode($result); 
+}
+
 ?>
